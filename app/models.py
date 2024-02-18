9360a36aa2f3ae5993c69d99edc979bb01e26c64
Tutur33 <arthur.puechberty@outlook.com>
2024-02-18 14:46:41 +0100

    add group

--- app/models.py
+++ app/models.py
@@ -3,13 +3,23 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 class Users(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    is_active = db.Column(db.Boolean, default=True)
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(64), index=True, unique=True)
+  password_hash = db.Column(db.String(128))
+  is_active = db.Column(db.Boolean, default=True)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+  def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+  def check_password(self, password):
+    return check_password_hash(self.password_hash, password)
+
+
+class Groups(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(64), index=True, unique=True)
+  description = db.Column(db.String(128))
+  private = db.Column(db.Boolean, default=True)
+  admins = db.Column(db.PickleType)
+  members = db.Column(db.PickleType, default=[]) 
+  is_active = db.Column(db.Boolean, default=True)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, request, flash, redirect, url_for
+from flask import render_template, request, flash, redirect, url_for, abort
 from app import app, db
 from werkzeug.security import generate_password_hash, check_password_hash
-from .models import Users
+from .models import Users, Groups
 from flask_login import current_user, login_user, logout_user, login_required
 
 
@@ -17,80 +17,121 @@ def about():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+  if request.method == 'POST':
+    username = request.form.get('username')
+    password = request.form.get('password')
 
-        user = Users.query.filter_by(username=username).first()
+    user = Users.query.filter_by(username=username).first()
 
-        if user and check_password_hash(user.password_hash, password):
-            login_user(user)
-            return redirect(url_for('home'))
-        else:
-            flash('Nom d\'utilisateur ou mot de passe incorrect')
+    if user and check_password_hash(user.password_hash, password):
+      login_user(user)
+      return redirect(url_for('home'))
+    else:
+      flash('Nom d\'utilisateur ou mot de passe incorrect')
 
-    return render_template('login.html')
+  return render_template('login.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+  if request.method == 'POST':
+    username = request.form.get('username')
+    password = request.form.get('password')
 
-        user = Users.query.filter_by(username=username).first()
+    user = Users.query.filter_by(username=username).first()
 
-        if user:
-            flash('Ce nom d\'utilisateur est déjà pris')
-        else:
-            new_user = Users(username=username, password_hash=generate_password_hash(password, method='pbkdf2:sha256'))
+    if user:
+      flash('Ce nom d\'utilisateur est déjà pris')
+    else:
+      new_user = Users(username=username, password_hash=generate_password_hash(password, method='pbkdf2:sha256'))
 
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user)
-            return redirect(url_for('home'))
+      db.session.add(new_user)
+      db.session.commit()
+      login_user(new_user)
+      return redirect(url_for('home'))
 
-    return render_template('register.html')
+  return render_template('register.html')
 
 
 @app.route('/logout')
 def logout():
-    logout_user()
-    return redirect(url_for('home'))
+  logout_user()
+  return redirect(url_for('home'))
 
 
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('dashboard.html', titre='Tableau de bord', current_user=current_user)
+  groupsPublic = Groups.query.filter_by(private=False).all()
+  myGroups = []
+  for group in Groups.query.all():
+    if current_user.id in group.admins:
+      myGroups.append(group)
+  print(myGroups)
+
+  return render_template('dashboard.html', titre='Tableau de bord', current_user=current_user, myGroups=myGroups, groupsPublic=groupsPublic)
 
 
 @app.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        delete = request.form.get('delete')
+  if request.method == 'POST':
+    username = request.form.get('username')
+    password = request.form.get('password')
+    delete = request.form.get('delete')
 
-        if username:
-            if current_user.username != username:
-                if Users.query.filter_by(username=username).first():
-                    flash('Ce nom d\'utilisateur est déjà pris')
-                    return redirect(url_for('profile'))
-                current_user.username = username
-                flash('Votre nom d\'utilisateur a été modifié.')
-        if password:
-            current_user.set_password(password)
-            flash('Votre mot de passe a été modifié.')
-        if delete:
-            db.session.delete(current_user)
-            logout_user()
-            flash('Votre compte a été supprimé.')
-            return redirect(url_for('home'))
+    if username:
+      if current_user.username != username:
+        if Users.query.filter_by(username=username).first():
+          flash('Ce nom d\'utilisateur est déjà pris')
+          return redirect(url_for('profile'))
+        current_user.username = username
+        flash('Votre nom d\'utilisateur a été modifié.')
+    if password:
+      current_user.set_password(password)
+      flash('Votre mot de passe a été modifié.')
+    if delete:
+      db.session.delete(current_user)
+      logout_user()
+      flash('Votre compte a été supprimé.')
+      return redirect(url_for('home'))
 
-        db.session.commit()
-        flash('Vos modifications ont été enregistrées.')
-        return redirect(url_for('profile'))
+    db.session.commit()
+    flash('Vos modifications ont été enregistrées.')
+    return redirect(url_for('profile'))
 
-    return render_template('profile.html', title='Profil')
+  return render_template('profile.html', title='Profil')
+    
+
+@app.route('/create_group', methods=['POST'])
+@login_required
+def create_group():
+  name = request.form.get('name')
+  description = request.form.get('description')
+  private = request.form.get('private')
+  if private == 'on':
+    private = True
+  else:
+    private = False
+
+  if name:
+    if Groups.query.filter_by(name=name).first():
+      flash('Ce nom de groupe est déjà pris')
+    else:
+      new_group = Groups(name=name, description=description, private=private, admins=[current_user.id])
+      db.session.add(new_group)
+      db.session.commit()
+      flash('Le groupe a été créé.')
+  else:
+    flash('Le nom du groupe est requis')
+
+  return redirect(url_for('dashboard'))
+
+
+@app.route('/group/<int:id>')
+@login_required
+def group(id):
+  group = Groups.query.get(id)
+  if group is None:
+      abort(404)
+  return render_template('group.html', group=group)
